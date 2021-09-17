760ac391fde42e7ed206cca5525741f6e248c845
maxlestage <maxlestage@icloud.com>
2021-09-17 13:12:46 +0200

    remove all images and end of likeCtrl

--- backend/controllers/like.js
+++ backend/controllers/like.js
@@ -1,40 +1,76 @@
 const Sauce = require('../models/Sauce');
 
 exports.userLikeOrDislikeSauce = (req, res) => {
-    const sauce = Sauce.findById(req.params.id);
+    const sauce = Sauce.findByIdAndUpdate(req.params.id);
     sauce.then((s) => {
         switch (req.body.like) {
             case 1:
-                Sauce.updateOne(
-                    { _id: s.id },
-                    {
-                        likes: s.likes + 1,
-                        $push: { usersLiked: req.body.userId },
-                    }
-                ).then(() => res.status(200).json(s));
-                console.log("J'aime");
+                if (!s.usersLiked.includes(req.body.userId)) {
+                    Sauce.updateOne(
+                        { _id: s.id },
+                        {
+                            likes: s.likes + 1,
+                            $push: { usersLiked: req.body.userId },
+                        }
+                    )
+
+                        .then(() => res.status(200).json(s))
+                        .catch((error) => res.status(400).json({ error }));
+                    console.log("J'aime cette sauce!");
+                } else {
+                    Sauce.updateOne()
+                        .then((sauces) => res.status(200).json(sauces))
+                        .catch((error) => res.status(400).json({ error }));
+                    console.log(
+                        "J'ai déjà donné mon avis sur cette sauce, je l'aime."
+                    );
+                }
                 break;
 
             case -1:
-                // Sauce.updateOne(
-                //     { _id: s.id },
-                //     {
-                //         dislikes: s.dislikes + 1,
-                //         $push: { usersDisliked: req.body.userId },
-                //     }
-                // ).then(() => res.status(200).json(s));
-                console.log('On aime plus du tout');
-                // expected output: "Mangoes and papayas are $2.79 a pound."
+                if (!s.usersDisliked.includes(req.body.userId)) {
+                    Sauce.updateOne(
+                        { _id: s.id },
+                        {
+                            dislikes: s.dislikes + 1,
+                            $push: { usersDisliked: req.body.userId },
+                        }
+                    )
+                        .then(() => res.status(200).json(s))
+                        .catch((error) => res.status(400).json({ error }));
+                    console.log("J'aime pas cette sauce");
+                } else {
+                    Sauce.updateOne()
+                        .then((sauces) => res.status(200).json(sauces))
+                        .catch((error) => res.status(400).json({ error }));
+                    console.log(
+                        "J'ai déjà donné mon avis sur cette sauce, je ne l'aime pas."
+                    );
+                }
                 break;
 
             case 0:
+                let update = {};
+                if (s.usersLiked.includes(req.body.userId)) {
+                    update = {
+                        likes: s.likes - 1,
+                        $pull: { usersLiked: req.body.userId },
+                    };
+                } else {
+                    update = {
+                        dislikes: s.dislikes - 1,
+                        $pull: { usersDisliked: req.body.userId },
+                    };
+                }
+                Sauce.updateOne({ _id: s.id }, update).then(() =>
+                    res.status(200).json(s)
+                );
                 console.log('Neutre');
-                // expected output: "Mangoes and papayas are $2.79 a pound."
                 break;
         }
     });
-
-    /* 
+};
+/* 
 - récupérer la sauce qui m'intéresse
 
 - si l'utilisateur a liké
@@ -62,4 +98,3 @@ exports.userLikeOrDislikeSauce = (req, res) => {
 - sauvegarder la sauce
 
 */
-};
